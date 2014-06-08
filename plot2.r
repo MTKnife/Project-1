6bbd8dfe7efbeed120442c8e7405dddcbb787702
MTKnife <sdorr@mtknife.net>
2014-06-08 19:12:48 -0400

    Scripts and PNG Plots for Project 1, Exploratory Data Analysis

---
+++ plot2.r
@@ -0,0 +1,28 @@
+# Load the graphics package.
+library("graphics")
+
+# Initialize variables.
+energyData <- data.frame()
+targetData <- data.frame()
+
+setwd("C:/text/Data Science/Exploratory Analysis/Project 1")
+
+# Read the entire data set.  I've chosen to read the whole thing, because
+# read.table is easier to use here than readLines.
+energyData <- read.table("household_power_consumption.txt", header = TRUE,
+  sep = ";", na.strings = "?")
+
+# Convert the dates into R date objects, subset the records for the appropriate
+# dates, and then convert the times into POSIXlt objects.  There are,
+# incidentally, no missing values in the targe range, which means we don't have
+# to worry about that problem.
+energyData$RDate <- as.Date(energyData$Date, "%d/%m/%Y")
+targetData <- energyData[energyData$RDate >= as.Date("2007-02-01") &
+  energyData$RDate <= as.Date("2007-02-02"), ]
+targetData$RTime <- strptime(paste(targetData$RDate, targetData$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# Draw Plot 2 and save it to file to "plot1.png".
+png(file = "plot2.png")
+with(targetData, (plot(RTime, Global_active_power, type = "l", xlab = "",
+  ylab = "Global Active Power (kilowatts)")))
+dev.off()
---
+++ plot3.r
@@ -0,0 +1,32 @@
+# Load the graphics package.
+library("graphics")
+
+# Initialize variables.
+energyData <- data.frame()
+targetData <- data.frame()
+
+setwd("C:/text/Data Science/Exploratory Analysis/Project 1")
+
+# Read the entire data set.  I've chosen to read the whole thing, because
+# read.table is easier to use here than readLines.
+energyData <- read.table("household_power_consumption.txt", header = TRUE,
+  sep = ";", na.strings = "?")
+
+# Convert the dates into R date objects, subset the records for the appropriate
+# dates, and then convert the times into POSIXlt objects.  There are,
+# incidentally, no missing values in the targe range, which means we don't have
+# to worry about that problem.
+energyData$RDate <- as.Date(energyData$Date, "%d/%m/%Y")
+targetData <- energyData[energyData$RDate >= as.Date("2007-02-01") &
+  energyData$RDate <= as.Date("2007-02-02"), ]
+targetData$RTime <- strptime(paste(targetData$RDate, targetData$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# Draw Plot 3 and save it to file to "plot1.png".
+png(file = "plot3.png")
+with(targetData, (plot(RTime, Sub_metering_1, type = "l", col = "black",
+  xlab = "", ylab ="Energy sub metering")))
+with(targetData, (lines(RTime, Sub_metering_2, col = "red")))
+with(targetData, (lines(RTime, Sub_metering_3, col = "blue")))
+legend("topright", lty = 1, col = c("black", "red", "blue"),
+  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))	
+dev.off()
